07edcbb9f15edd957c3dfc1efdee03c3023973cd
Anne-Marie <a.magnan@imperial.ac.uk>
2015-01-23 09:00:07 +0000

    improve style + add control regions

--- analysis/test/plotVariable.cpp
+++ analysis/test/plotVariable.cpp
@@ -18,102 +18,74 @@
 #include "THStack.h"
 #include "TLegend.h"
 #include "TGraphAsymmErrors.h"
-
+#include "TLatex.h"
 
 #include "TDRStyle.h"
 #include "utilities.hh"
 
+enum SELECTION{
+  SIGNAL=1,
+  MuMu=2,
+  LNu=3,
+  Top=4
+};
+
 int main( int argc, char** argv )
 {//main
 
-  std::string varName = "dijet_M";
-  std::string path = "/data/shared/meridian/Long_Exercise_Hinvisible/Skims/";
-  //std::string path = "../../data/Skims8TeV/";
+  if (argc<7) {
+    std::cout << " Usage: " << argv[0] << " "
+	      << "<variable> <hist title> "
+	      << "<nbin> <binmin> <binmax> "
+	      << "<selection: 1=signal, 2=mumu, 3=lnu, 4=top>"
+	      << std::endl;
+    return 1;
+  }
+
+  std::string varName = argv[1];
+  std::string title = argv[2];
+  double nBins = atoi(argv[3]);
+  double xmin  = atof(argv[4]);
+  double xmax = atof(argv[5]);
+  SELECTION sel = SELECTION(atoi(argv[6]));
+
+  //std::string path = "/data/shared/meridian/Long_Exercise_Hinvisible/Skims/";
+  std::string path = "./skims/";
+
 
   double lumiData = 10000;//in pb-1
 
-  double xmin  = 1000;
-  double xmax = 4000;
-  double nBins = 100;
-  bool dology = true;
-
-  std::string title = ";M_{jj} (GeV); events/30 GeV";
+  bool dology = false;
 
   const unsigned nCat = 4;
   std::string bkgcat[nCat] = {"Top","VV","W+jets","Z+jets"};
 
-  double dataSF[nCat] = {0.84,1,0.7,0.67};//*0.6/0.0334};
+  double dataSF[nCat] = {0.84,1,0.7,0.67};//*0.2/0.0334};
 
   std::vector<std::string> files;
-  //8 TeV
-  /*
-  files.push_back("MC_TTJets");
-  files.push_back("MC_T-tW");
-  files.push_back("MC_Tbar-tW");
-  files.push_back("MC_SingleT-s-powheg-tauola");
-  files.push_back("MC_SingleTBar-s-powheg-tauola");
-  files.push_back("MC_SingleT-t-powheg-tauola");
-  files.push_back("MC_SingleTBar-t-powheg-tauola");
-  files.push_back("MC_WW-pythia6-tauola");
-  files.push_back("MC_WZ-pythia6-tauola");
-  files.push_back("MC_ZZ-pythia6-tauola");
-  files.push_back("MC_W1JetsToLNu_enu");
-  files.push_back("MC_W2JetsToLNu_enu");
-  files.push_back("MC_W3JetsToLNu_enu");
-  files.push_back("MC_W4JetsToLNu_enu");
-  files.push_back("MC_WJetsToLNu-v1_enu");
-  files.push_back("MC_WJetsToLNu-v2_enu");
-  files.push_back("MC_W1JetsToLNu_munu");
-  files.push_back("MC_W2JetsToLNu_munu");
-  files.push_back("MC_W3JetsToLNu_munu");
-  files.push_back("MC_W4JetsToLNu_munu");
-  files.push_back("MC_WJetsToLNu-v1_munu");
-  files.push_back("MC_WJetsToLNu-v2_munu");
-  files.push_back("MC_W1JetsToLNu_taunu");
-  files.push_back("MC_W2JetsToLNu_taunu");
-  files.push_back("MC_W3JetsToLNu_taunu");
-  files.push_back("MC_W4JetsToLNu_taunu");
-  files.push_back("MC_WJetsToLNu-v1_taunu");
-  files.push_back("MC_WJetsToLNu-v2_taunu");
-  files.push_back("MC_EWK-W2jminus_enu");
-  files.push_back("MC_EWK-W2jplus_enu");
-  files.push_back("MC_EWK-W2jminus_munu");
-  files.push_back("MC_EWK-W2jplus_munu");
-  files.push_back("MC_EWK-W2jminus_taunu");
-  files.push_back("MC_EWK-W2jplus_taunu");
-  //use DY ignoring muons for Znunu
-  files.push_back("MC_DYJetsToLL");
-  //files.push_back("MC_DYJetsToLL_PtZ-100-madgraph");
-  files.push_back("MC_DY1JetsToLL");
-  files.push_back("MC_DY2JetsToLL");
-  files.push_back("MC_DY3JetsToLL");
-  files.push_back("MC_DY4JetsToLL");
-  //files.push_back("MC_ZJetsToNuNu_100_HT_200");
-  //files.push_back("MC_ZJetsToNuNu_200_HT_400");
-  //files.push_back("MC_ZJetsToNuNu_400_HT_inf");
-  //files.push_back("MC_ZJetsToNuNu_50_HT_100");
-  //files.push_back("MC_EWK-Z2j");
-  files.push_back("MC_EWK-Z2j_iglep");
-  */
   files.push_back("TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola");
   //files.push_back("TT_Tune4C_13TeV-pythia8-tauola");
   files.push_back("T_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola");
   files.push_back("Tbar_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola");
   files.push_back("ZZTo4L_Tune4C_13TeV-powheg-pythia8");
-  files.push_back("WJetsToLNu_13TeV-madgraph-pythia8-tauola");
+  //files.push_back("WJetsToLNu_13TeV-madgraph-pythia8-tauola");
   files.push_back("WJetsToLNu_HT-100to200_Tune4C_13TeV-madgraph-tauol");
   files.push_back("WJetsToLNu_HT-200to400_Tune4C_13TeV-madgraph-tauola");
   files.push_back("WJetsToLNu_HT-400to600_Tune4C_13TeV-madgraph-tauola");
   files.push_back("WJetsToLNu_HT-600toInf_Tune4C_13TeV-madgraph-tauola");
-  files.push_back("ZJetsToNuNu_HT-100to200_Tune4C_13TeV-madgraph-tauola");
-  files.push_back("ZJetsToNuNu_HT-200to400_Tune4C_13TeV-madgraph-tauola");
-  files.push_back("ZJetsToNuNu_HT-400to600_Tune4C_13TeV-madgraph-tauola");
-  files.push_back("ZJetsToNuNu_HT-600toInf_Tune4C_13TeV-madgraph-tauola");
-  //files.push_back("DYJetsToLL_M-50_13TeV-madgraph-pythia8");
-  //files.push_back("DYJetsToLL_M-50_HT-100to200_Tune4C_13TeV-madgraph-tauola");
-  //files.push_back("DYJetsToLL_M-50_HT-200to400_Tune4C_13TeV-madgraph-tauola");
-  //files.push_back("DYJetsToLL_M-50_HT-400to600_Tune4C_13TeV-madgraph-tauola");
-  //files.push_back("DYJetsToLL_M-50_HT-600toInf_Tune4C_13TeV-madgraph-tauola");
+  if (sel==SELECTION::SIGNAL){
+    files.push_back("ZJetsToNuNu_HT-100to200_Tune4C_13TeV-madgraph-tauola");
+    files.push_back("ZJetsToNuNu_HT-200to400_Tune4C_13TeV-madgraph-tauola");
+    files.push_back("ZJetsToNuNu_HT-400to600_Tune4C_13TeV-madgraph-tauola");
+    files.push_back("ZJetsToNuNu_HT-600toInf_Tune4C_13TeV-madgraph-tauola");
+  }
+  else {
+    files.push_back("DYJetsToLL_M-50_13TeV-madgraph-pythia8");
+    files.push_back("DYJetsToLL_M-50_HT-100to200_Tune4C_13TeV-madgraph-tauola");
+    files.push_back("DYJetsToLL_M-50_HT-200to400_Tune4C_13TeV-madgraph-tauola");
+    files.push_back("DYJetsToLL_M-50_HT-400to600_Tune4C_13TeV-madgraph-tauola");
+    files.push_back("DYJetsToLL_M-50_HT-600toInf_Tune4C_13TeV-madgraph-tauola");
+  }
 
   std::string sigfile = "VBF_HToInv_M-125_13TeV_powheg-pythia6";
 
@@ -128,15 +100,17 @@ int main( int argc, char** argv )
 
   for (unsigned iF(0); iF<nMC;++iF){//loop on files
     if (iF<3) {color[iF] = 5; style[iF] = 1001; label[iF] = bkgcat[0];}
-    else if (iF<4) {color[iF] = 4; style[iF] = 3004; label[iF] = bkgcat[1];}
-    else if (iF<9) {color[iF] = 2; style[iF] = 3005; label[iF] = bkgcat[2];}
-    else {color[iF] = 3; style[iF] = 3006; label[iF] = bkgcat[3];}
+    else if (iF<4) {color[iF] = 3; style[iF] = 3004; label[iF] = bkgcat[1];}
+    else if (iF<8) {color[iF] = 2; style[iF] = 3005; label[iF] = bkgcat[2];}
+    else {color[iF] = 7; style[iF] = 3006; label[iF] = bkgcat[3];}
   };
 
-  color[nMC] = 7;
+  color[nMC] = 4;
   style[nMC] = 1001;
 
   TCanvas *myc = new TCanvas("myc","myc",1);
+  SetTdrStyle();
+
 
   TH1F *hist[nMC+1];
   TH1F *bkg[4]={0,0,0,0};
@@ -154,14 +128,14 @@ int main( int argc, char** argv )
     TTree *tree = (TTree*)gDirectory->Get("LightTree");
     if (!tree) return 1;
 
-    bool isDY = iF<nMC && files[iF].find("DY")!=files[iF].npos;
-    
     double weight = getNormalisationFactor(lumiData,iF<nMC?files[iF]:sigfile);
 
     std::ostringstream lselection,lname;
     std::string signal;
-    if (!isDY) signal = "(l1met>=70 && nvetomuons==0 && nvetoelectrons==0)";
-    else signal = "(l1met>=70 && nselmuons==2)";//dummy sel
+    if (sel==SELECTION::SIGNAL) signal = "(l1met>=60 && nvetomuons==0 && nvetoelectrons==0 && dijet_M>1120 && dijet_deta>4.1 && metnomu_significance>4.9 && alljetsmetnomu_mindphi>2.2 && passtrigger>0.5)";
+    else if (sel==SELECTION::MuMu) signal = "(l1met>=60 && nselmuons==2 && dijet_M>1120 && dijet_deta>4.1 && metnomu_significance>4.9 && alljetsmetnomu_mindphi>2.2)";
+    else if (sel==SELECTION::LNu) signal = "(l1met>=60 && ((nselelectrons==1  && nvetomuons==0) || (nselmuons==1  && nvetoelectrons==0) || (ntaus==1 && nvetoelectrons==0 && nvetomuons==0))&& dijet_M>1120 && dijet_deta>4.1 && metnomu_significance>4.9 && alljetsmetnomu_mindphi>2.2)";
+    else if (sel==SELECTION::Top) signal = "(l1met>=60 && nselelectrons==1  && nselmuons==1 && dijet_M>1120 && dijet_deta>4.1 && metnomu_significance>4.9)";
     lselection << signal << "*" << weight;
     //for 8TeV trees
     //lselection << signal ;
@@ -175,9 +149,9 @@ int main( int argc, char** argv )
     tree->Draw(lname.str().c_str(),lselection.str().c_str(),"");
 
     hist[iF]->SetLineColor(color[iF]);
-    hist[iF]->SetFillColor(color[iF]);
-    hist[iF]->SetFillStyle(style[iF]);
     if (iF<nMC){
+      hist[iF]->SetFillColor(color[iF]);
+      hist[iF]->SetFillStyle(style[iF]);
       for (unsigned iC(0);iC<nCat;++iC){
 	if (label[iF] != bkgcat[iC]) continue;
 	if (!bkg[iC]) bkg[iC] = (TH1F*)hist[iF]->Clone(bkgcat[iC].c_str());
@@ -191,7 +165,7 @@ int main( int argc, char** argv )
 
   myc->cd();
   gPad->SetLogy(dology);
-  TLegend *leg = new TLegend(0.7,0.7,0.94,0.94);
+  TLegend *leg = new TLegend(0.6,0.5,0.89,0.89);
   leg->SetFillColor(10);
   //myc->Divide(2,2);
   
@@ -215,10 +189,18 @@ int main( int argc, char** argv )
     bkg[newid]->Draw(iC==0? "hist" : "histsame");
     leg->AddEntry(bkg[newid],bkgcat[newid].c_str(),"F");
   }
-  hist[nMC]->Draw("same");
+  hist[nMC]->SetLineWidth(2);
+  hist[nMC]->Draw("histsame");
   leg->AddEntry(hist[nMC],"VBF H, m_{H}=125 GeV","F");
   leg->Draw("same");
   myc->Update();
+
+  TLatex lat;
+  char buf[200];
+  lat.DrawLatexNDC(0.1,0.95,"CMSDAS@BARI 2015, CMS simulation");
+  sprintf(buf,"#sqrt{s}=13 TeV, L=%3.0f fb^{-1}",lumiData/1000.);
+  lat.DrawLatexNDC(0.15,0.85,buf);
+
   std::ostringstream lsave;
   lsave.str("");
   lsave << "PLOTS/" << varName;
